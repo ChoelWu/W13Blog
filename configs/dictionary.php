d4d3a057f709b98523fb6455c3e97716354b3f4f
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2019-01-09 16:40:41 +0800

    accomplish
    accomplish

---
+++ configs/dictionary.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Choel
+ * Date: 2019/1/9
+ * Time: 15:15
+ */
+
+return [
+    'table_name' => [
+        'value1' => 'key1',
+        'value2' => 'key2'
+    ]
+];
