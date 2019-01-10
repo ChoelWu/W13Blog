add6278ad257d0bf82ee26fcae7787e78144a3c1
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2019-01-10 18:34:56 +0800

    [channel]
    [channel] update

--- app/action/admin/ChannelController.class.php
+++ app/action/admin/ChannelController.class.php
@@ -15,21 +15,31 @@ class ChannelController extends CommonController
         $sql = 'SELECT * FROM `blog_channel`;';
         $channel_list = $this->db->getAll($sql);
         $type = getConfig('dictionary.channel.type', '未知');
-        $this->assign(['channel_list' => $channel_list, 'type' => $type]);
+        $status = getConfig('dictionary.common.status', '未知');
+        $this->assign([
+            'channel_list' => $channel_list,
+            'type' => $type,
+            'status' => $status
+        ]);
         $this->display('tpl/default/admin/channel/index.html');
     }
 
     public function add()
     {
+        include APP_PATH . '/function/tree.php';
         $sql = 'SELECT * FROM `blog_channel`;';
         $channel_list = $this->db->getAll($sql);
+        $channelTree = getChannelTree($channel_list, 0, 1);
         $type_list = getConfig('dictionary.channel.type', '未知');
         $status_list = getConfig('dictionary.common.status', '未知');
         $this->assign([
             'type_list' => $type_list,
             'status_list' => $status_list,
-            'channel_list' => $channel_list
+            'channelTree' => $channelTree
         ]);
+//        echo '<pre>';
+//        var_dump($channelTree);
+//        echo '<pre>';
         $this->display('tpl/default/admin/channel/add.html');
     }
 
--- app/function/tree.php
+++ app/function/tree.php
@@ -6,15 +6,23 @@
  * Time: 16:54
  */
 
-function getChannelTree($rawChannelArr) {
-    $tree = ['0' => 0];
-    $level = 0;
-    while(true) {
-        $level ++;
-        foreach($rawChannelArr as $rawChannel) {
-            if($rawChannel['level'] == $level) {
-                $tree[] = $rawChannel;
-            }
+/**
+ * 获取树形菜单
+ * @param $menuArr
+ * @param $parentId
+ * @param $level
+ * @return array
+ */
+function getChannelTree($menuArr, $parentId, $level)
+{
+    $result_arr = [];
+
+    foreach ($menuArr as $menuItem) {
+        if ($menuItem['channel_parent_id'] == $parentId && $menuItem['channel_level'] == $level) {
+            $menuItem['children'] = getChannelTree($menuArr, $menuItem['id'], $level + 1);
+            $result_arr[] = $menuItem;
         }
     }
+
+    return $result_arr;
 }
